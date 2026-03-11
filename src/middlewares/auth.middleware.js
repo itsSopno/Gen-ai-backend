bf0ffe4d18306219da460a9057462b9fd726df87
unknown <>
2026-03-12 02:53:10 +0600

    save changes

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,10 +1,14 @@
 const jwt = require("jsonwebtoken")
-const tokeblacklismodel = re
-function authUser(req, res, next) {
+const tokenBlacklistModel = require("../Models/Blacklist.model")
+async function authUser(req, res, next) {
     const token = req.cookies.token
     if (!token) {
         return res.status(401).json({ message: "Unauthorized" })
 
+    }
+    const tokeblacklismodel = await tokenBlacklistModel.findOne({ token })
+    if (tokeblacklismodel) {
+        return res.status(401).json({ message: "token is blacklisted" })
     }
     try {
         const decode = jwt.verify(token.process.env.JWT_SECRET,)
